97fdc3a251aded1b9ea4a24130d4be3d290da30f
mike <mikeshai101@gmail.com>
2013-01-30 11:39:02 +0200

    Added Remarks

--- Playlist.cs
+++ Playlist.cs
@@ -7,6 +7,8 @@ namespace Clipamon.Model.Videos
         public ulong? PlaylistID { get; set; }
         public string Name { get; set; }
 
+		//$git tag -a vX.Y -m 'version X.Y'
+		//git push --tags
 		int timp=rand()*100;
 		int tmp2 =tmp1*sin();
     }
